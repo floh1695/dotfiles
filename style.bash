b10a649a73d54f8eee9750f70366080b5a76e473
Charles Bayley <cbayley95@gmail.com>
2015-03-19 22:25:22 +0000

    Changed style   * Needs to be finished still   * Pushing to work on desktop

--- style.bash
+++ style.bash
@@ -3,7 +3,14 @@
 srcdir="$HOME/.etcbash"
 source "$srcdir/colors.bash"
 prompt_set() {
-  export PS1="${Yellow}\u${Cyan}@${Yellow}\h ${White}[${Green}\W${White}] ${Red}\$${Yellow}-> ${Color_Off}"
+  PS1="${Yellow}\u${Cyan}@${Yellow}\h ${White}[${Green}\W${White}] "
+  if [ $? -eq 0 ]; then
+    PS1+="${Green}"
+  else
+    PS1+="${Red}"
+  fi
+  PS1+="\$${Yellow}-> ${Color_Off}"
+  export PS1
   export PS2="${Yellow}-> ${Color_Off}"
 }
 export PROMPT_COMMAND="prompt_set"
