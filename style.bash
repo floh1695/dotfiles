bbc126d3ed55c26f3349eaca6489d913f1773ab3
Charles Bayley <cbayley95@gmail.com>
2015-03-20 00:15:47 -0700

    I made it pring out a green $ if the last command was a success and a red one otherwise

--- style.bash
+++ style.bash
@@ -3,8 +3,9 @@
 srcdir="$HOME/.etcbash"
 source "$srcdir/colors.bash"
 prompt_set() {
+  PS1_EXIT_STATUS="${?}"
   PS1="${Yellow}\u${Cyan}@${Yellow}\h ${White}[${Green}\W${White}] "
-  if [ $? -eq 0 ]; then
+  if [ ${PS1_EXIT_STATUS} -eq 0 ]; then
     PS1+="${Green}"
   else
     PS1+="${Red}"
