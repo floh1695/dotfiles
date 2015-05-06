bdf084fdaa3d6ebf1898cf4ebc68c3e83c5f4a47
Charles Bayley <cbayley95@gmail.com>
2015-05-06 14:29:05 -0700

    Made install force selected change

--- install.bash
+++ install.bash
@@ -22,5 +22,5 @@ cp -rf "general" "${envir_dir}"
 # Select an environment
 #     TODO: Make this a user selected choice.
 #           For now it should be fine to hard code the general environment
-ln -sri "${envir_dir}/general" "${selected_dir}"
+ln -srf "${envir_dir}/general" "${selected_dir}"
 
