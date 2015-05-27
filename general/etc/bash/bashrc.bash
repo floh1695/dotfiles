16fb3bcd7ace3c0b1c0555ddb04d7602e76fc8e9
Charles Bayley <cbayley95@gmail.com>
2015-05-27 14:36:11 -0700

    Quickly threw in something for GCCs color support

--- general/etc/bash/bashrc.bash
+++ general/etc/bash/bashrc.bash
@@ -3,6 +3,8 @@
 # includes
 source "style.bash"
 
+GCC_COLORS="error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01"
+
 # path
 if [[ ! "$PATH" == ?(*:)"${HOME}/bin"?(:*) ]]; then
   PATH="${HOME}/bin:${PATH}"
